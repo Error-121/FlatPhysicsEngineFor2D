97d2fc0e45582a2f64c192484e69b9f71f0a7dbb
Error-121 <Frederiklandgraf@gmail.com>
2025-05-07 12:01:40 +0200

    Add project files.

---
+++ FlatPhysicsEngineFor2D/FlatVector.cs
@@ -0,0 +1,65 @@
+﻿namespace FlatPhysicsEngineFor2D
+{
+    public class FlatVector
+    {
+        public readonly float X;
+		public readonly float Y;
+
+		public static readonly FlatVector Zero = new FlatVector(0f, 0f);
+
+		public FlatVector(float x, float y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public static FlatVector operator +(FlatVector a, FlatVector b)
+		{
+			return new FlatVector(a.X + b.X, a.Y + b.Y);
+		}
+
+		public static FlatVector operator -(FlatVector a, FlatVector b)
+		{
+			return new FlatVector(a.X - b.X, a.Y - b.Y);
+		}
+
+		public static FlatVector operator -(FlatVector vector)
+		{
+			return new FlatVector(-vector.X, -vector.Y);
+		}
+
+		public static FlatVector operator *(FlatVector vector, float scalar)
+		{
+			return new FlatVector(vector.X * scalar, vector.Y * scalar);
+		}
+
+		public static FlatVector operator /(FlatVector vector, float scalar)
+		{
+			return new FlatVector(vector.X / scalar, vector.Y / scalar);
+		}
+
+		public bool Equals(FlatVector other)
+		{
+			return X == other.X && Y == other.Y;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is FlatVector other)
+			{
+				return Equals(other);
+			}
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return (this.X, this.Y).GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			return $"(X: {this.X}, Y: {this.Y})";
+		}
+	}
+}
---
+++ FlatPhysicsEngineFor2D/FlathMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FlatPhysicsEngineFor2D
+{
+	public static class FlathMath
+	{
+		public static float Length(FlatVector vector)
+		{
+			
+		}
+
+		public static float Distance(FlatVector a, FlatVector b)
+		{
+			return Length(a - b);
+		}
+
+
+	}
+}
