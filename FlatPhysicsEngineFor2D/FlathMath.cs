8992f6f51c1dfd159767ad51ae41792f74860963
Error-121 <Frederiklandgraf@gmail.com>
2025-05-07 14:51:38 +0200

    Enhance FlathMath with vector operations
    Updated the `FlathMath` class to simplify the `Length` method and modify the `Distance` method for better performance. Added new methods: `Normalize`, `Dot`, and `Cross` for comprehensive vector operations in 2D. Removed outdated comments for cleaner code.

--- FlatPhysicsEngineFor2D/FlathMath.cs
+++ FlatPhysicsEngineFor2D/FlathMath.cs
@@ -4,14 +4,42 @@ namespace FlatPhysicsEngineFor2D
 {
 	public static class FlathMath
 	{
+
 		public static float Length(FlatVector vector)
 		{
-			
+			//float deltaX = vector.X - 0f;
+			//float deltaY = vector.Y - 0f;
+			//return (float)Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+			return (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
 		}
 
 		public static float Distance(FlatVector a, FlatVector b)
 		{
-			return Length(a - b);
+			float deltaX = a.X - b.X;
+			float deltaY = a.Y - b.Y;
+			return (float)Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+		}
+
+		public static FlatVector Normalize(FlatVector vector)
+		{
+			float length = Length(vector);
+			//float x = vector.X / length;
+			//float y = vector.Y / length;
+			//return new FlatVector(x, y);
+			return new FlatVector(vector.X / length, vector.Y / length);
+		}
+
+		public static float Dot(FlatVector a, FlatVector b)
+		{
+			//a · b = ax × bx + ay × by
+			return a.X * b.X + a.Y * b.Y;
+		}
+
+		public static float Cross(FlatVector a, FlatVector b)
+		{
+			//cx og zy er fjernet da de ikke er relevante i 2D
+			//cz = axby − aybx
+			return a.X * b.Y - a.Y * b.X;
 		}
 
 
